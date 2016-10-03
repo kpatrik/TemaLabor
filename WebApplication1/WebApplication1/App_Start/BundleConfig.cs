7f94fb1e1676cdd529c949e2b0a7170335d8b5d0
Dávid <Dvid@DESKTOP-23FUSAE>
2016-10-03 17:27:02 +0200

    asdasd

--- WebApplication1/WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/WebApplication1/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace WebApplication1
         //jo lenne ha müssssssss
         // hello
         //ló kacsa
+        //látod???
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
