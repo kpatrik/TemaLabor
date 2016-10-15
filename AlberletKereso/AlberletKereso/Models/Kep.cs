cc1e2936c69d3ada3e620ff119e8370ea4c9b3c9
Dávid <Dvid@DESKTOP-23FUSAE>
2016-10-16 01:13:50 +0200

    a felhasznalókülön látja a hirdetéseit

---
+++ AlberletKereso/AlberletKereso/Models/Kep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AlberletKereso.Models
+{
+    public class Kep { 
+        public Kep() { }
+
+        public Kep(string url, string filename, Alberlet alberlet)
+        {
+            Url = url;
+            Alberlet = alberlet;
+            FileName = filename;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int KepId { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public string FileName { get; set; }
+   
+        public Alberlet Alberlet { get; set; }
+
+    }
+}
