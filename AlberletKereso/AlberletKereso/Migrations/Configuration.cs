2d22c50be398eadce6d5f48e502c7dc053b0d40a
balazs9510 <95berta.balazs@gmail.com>
2016-10-24 11:49:31 +0200

    kicsi db

--- AlberletKereso/AlberletKereso/Migrations/Configuration.cs
+++ AlberletKereso/AlberletKereso/Migrations/Configuration.cs
@@ -18,51 +18,40 @@ namespace AlberletKereso.Migrations
 
         protected override void Seed(AlberletKereso.Models.ApplicationDbContext context)
         {
-            
-            if (!(context.Users.Any(u => u.UserName == "95berta.balazs@gmail.com")))
-            {
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-                var userToInsert = new ApplicationUser {
-                    UserName = "95berta.balazs@gmail.com", Email = "95berta.balazs@gmail.com" };
-                userManager.Create(userToInsert, "Valami_123");
-            }
-            if (!(context.Users.Any(u => u.UserName == "patrik5000@gmail.com")))
-            {
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-                var userToInsert = new ApplicationUser { UserName = "patrik5000@gmail.com", Email = "patrik5000@gmail.com" };
-                userManager.Create(userToInsert, "Valami_123");
-            }
-            if (!(context.Users.Any(u => u.UserName == "moczardavid@gmail.com")))
-            {
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-                var userToInsert = new ApplicationUser { UserName = "moczardavid@gmail.com", Email = "moczardavid@gmail.com" };
-                userManager.Create(userToInsert, "Valami_123");
-            }
-            if (!(context.Users.Any(u => u.UserName == "marci-95@hotmail.com")))
-            {
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-                var userToInsert = new ApplicationUser { UserName = "marci-95@hotmail.com", Email = "marci-95@hotmail.com" };
-                userManager.Create(userToInsert, "Valami_123");
-            }
+            initUser(context,"95berta.balazs@gmail.com");
+            initUser(context, "patrik5000@gmail.com");
+            initUser(context, "moczardavid@gmail.com");
+            initUser(context, "marci"+"-"+"95"+"@hotmail.com");
+            initUser(context, "kaki@kaki.com");
+
+
             if(!(context.Alberletek.Any(a=> a.AlberletId==1)))
-            {
-                var user = context.Users.Where(u=> u.UserName == "marci-95@hotmail.com").FirstOrDefault();
-                  var albi = new Alberlet("Arany utca 9.", 2, 0, 1, 23, 50000, false,user);
-                context.Alberletek.Add(albi);
-                context.SaveChanges();
-            }
-            if (!(context.Alberletek.Any(a => a.AlberletId == 1)))
-            {
-                var user = context.Users.Where(u => u.UserName == "moczardavid@gmail.com").FirstOrDefault();
-                var albi = new Alberlet("Madách utca 2.", 3, 2, 1, 50, 100000, false, user);
-                context.Alberletek.Add(albi);
-                context.SaveChanges();
-            }
+              {
+                  var user = context.Users.Where(u=> u.UserName == "kaki@kaki.com").FirstOrDefault();
+                    var albi = new Alberlet("Arany utca 9.", 2, 0, 1, 23, 50000, false,user);
+                  context.Alberletek.Add(albi);
+
+              }
+              if (!(context.Alberletek.Any(a => a.AlberletId == 1)))
+              {
+                  var user = context.Users.Where(u => u.UserName == "moczardavid@gmail.com").FirstOrDefault();
+                  var albi = new Alberlet("Madách utca 2.", 3, 2, 1, 50, 100000, false, user);
+                  context.Alberletek.Add(albi);
+
+              }
 
         }
+        private void initUser(AlberletKereso.Models.ApplicationDbContext context,string mail)
+        {
+            if (!(context.Users.Any(u => u.UserName .Equals(mail) )))
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var userToInsert = new ApplicationUser { UserName = mail , Email = mail };
+                userManager.Create(userToInsert, "Password@123");
+                userManager.Dispose();
+            }
+        }
     }
+    
 }
